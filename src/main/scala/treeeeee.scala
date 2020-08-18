40cda10775df5a685d08cf73037dd979c023dcf9
Ruta <ruta.grigorjeva@gmail.com>
2020-08-18 12:05:35 +0200

    Tree

--- src/main/scala/treeeeee.scala
+++ src/main/scala/treeeeee.scala
@@ -3,5 +3,5 @@ object treeeeee extends App{
     for ("3" <- Array("  *", " ##", "^^^")) println
   }
 
-  //I cant think a way to assing the tree to Int 3 :(, there is always mismatch in expected type
+  //I cant think a way to assign the tree to Int 3 :(, there is always mismatch in expected type
 }
